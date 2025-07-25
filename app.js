213f2b9c2aad6d1d9f5266f373f136fd660f1e57
bernard777 <ndongoambassa7@gmail.com>
2025-07-25 11:04:10 +0200

    configuration principale

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur l’API Cadok');
+});
+
+module.exports = app;
